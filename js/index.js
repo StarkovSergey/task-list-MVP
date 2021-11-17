32e31c404fe600d75aef66b6343d9a844fd4754d
Starkov Sergey <starkovsr@gmail.com>
2021-11-17 17:46:06 +0300

    feat: add util-functions

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ let tasks = [
     isDone: true,
   },
 ];
+
+const taskTemplateElement =document.querySelector('#task-template').content;
+
+const mainElement = document.querySelector('.main');
+const newTaskElement = mainElement.querySelector('.new-task');
+const addTaskButtonElement = mainElement.querySelector('.add-task-button');
+const clearTaskButtonElement = mainElement.querySelector('.clear-task-button');
+const tasksListElement = mainElement.querySelector('.tasks');
+
+const addNewTask = (title) => (tasks.push({
+  id: nanoid(),
+  title,
+  isDone: false,
+}));
+
+const clearTasks = () => { // можно было записать в одну строку как: (tasks = []);
+  tasks = [];
+}
+
+const completeTask = (id) => {
+  const existTask = task.find((task) => task.id === id);
+  existTask.isDone = !existTask.isDone;
+}
