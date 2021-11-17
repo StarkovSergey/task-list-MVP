87d10e4e11bfb4930a8a26704725e0e943abe32b
Starkov Sergey <starkovsr@gmail.com>
2021-11-17 17:23:20 +0300

    feat: add nanoid and test-date

---
+++ js/index.js
@@ -0,0 +1,15 @@
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js';
+
+
+let tasks = [
+  {
+    id: nanoid(),
+    title: 'take out the trash',
+    isDone: false,
+  },
+  {
+    id: nanoid(),
+    title: 'study Git',
+    isDone: true,
+  },
+];
